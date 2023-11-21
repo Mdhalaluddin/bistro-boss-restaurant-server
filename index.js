0f99957e3bca8552c4b3ded8214e306227e367ce
Halal1122 <mdhealaluddin505@gmail.com>
2023-11-22 04:04:01 +0600

    admin added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const post = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
- 
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -36,92 +36,116 @@ async function run() {
 
 
     // middlewares
-    const verifyToken = (req, res, next)=>{
-      console.log(req.headers);
-      if(!req.headers.authorization){
-        return res.status(401).send({message: 'forbidden access'});
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
       }
-      const token = req.headers.authorization.split(' ')[1]; 
-      jwt.verify(token, process.env.JWT_TOKEN, (err, decoded)=>{
-        if(err){
-          return res.status(401).send({message: 'forbidden access'})
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
         }
         req.decoded = decoded;
         next();
       })
-    } 
+    }
+    // use verify admin after verifyToken
+    const verifyAdmin = async(req, res, next)=>{
+      const email = req.decoded.email;
+      const query ={email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      next();
+    }
+
+
+    app.get('/users/admin/:email',verifyToken, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({admin})
+    })
     // jwt
-    app.post('/jwt', async(req, res)=>{
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      
-      const token = jwt.sign(user, process.env.JWT_TOKEN,{
+      const token = jwt.sign(user, process.env.JWT_TOKEN, {
         expiresIn: '1h'
-      }) 
-      res.send({token})
+      })
+      res.send({ token })
     })
 
     // users api
-    app.get('/users', async(req, res)=>{
-      
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray()
       res.send(result)
     })
     // user date
-    app.post('/users', async(req, res)=>{
+    app.post('/users', async (req, res) => {
       const user = req.body;
       console.log('new user', user);
-      const query = {email: user.email} 
+      const query = { email: user.email }
       const existingUser = await userCollection.findOne(query)
-      if(existingUser){
-       return res.send({message: 'user already existing', instatedId: null})
+      if (existingUser) {
+        return res.send({ message: 'user already existing', instatedId: null })
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
-    app.delete('/users/:id', async(req, res)=>{
+    app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
-      const query= {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
-    app.patch('/users/admin/:id', async(req, res)=>{
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const updateDoc ={
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
         $set: {
           role: 'admin'
         }
       }
-      const result= await userCollection.updateOne(filter, updateDoc);
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
     // menus data 
-    app.get('/menu', async(req, res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result)
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result)
     })
-    
-    app.get('/reviews', async(req, res)=>{
-        const result = await reviewsCollection.find().toArray();
-        res.send(result)
+
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result)
     })
     // add to cart
-    app.get('/carts', async(req, res)=>{
+    app.get('/carts', async (req, res) => {
       const email = req.query.email;
-      const query = {email: email}
+      const query = { email: email }
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.post('/carts', async(req, res)=>{
+    app.post('/carts', async (req, res) => {
       const user = req.body;
       console.log('new food cart and user', user);
       const result = await cartCollection.insertOne(user);
       res.send(result)
     });
-    app.delete('/carts/:id', async(req, res)=>{
+    app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result)
     })
@@ -138,10 +162,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('boss is sitting')
+app.get('/', (req, res) => {
+  res.send('boss is sitting')
 })
-app.listen(post,()=>{
-    console.log(`Bistro boss is sitting on post ${post}`)
+app.listen(post, () => {
+  console.log(`Bistro boss is sitting on post ${post}`)
 })
 
